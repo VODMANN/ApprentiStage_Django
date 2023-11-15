6ba06fcceaeeb7b8ced50a08b2deed75a95a38b5
sami53tk <rochdi53sami@gmail.com>
2023-11-15 11:41:35 +0100

    utilisation .env

--- ApprentiStageDjango/ApprentiStageDjango/settings.py
+++ ApprentiStageDjango/ApprentiStageDjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +83,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'apprentiStageDjango',
-        'USER': 'root',
-        'PASSWORD': 'MLKqsd002',
-        'HOST': '127.0.0.1',   # Set to the address of your database
-        'PORT': '3306',        # Leave as 3306, the default MySQL port
+        'USER': config('USERBD'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),   # Set to the address of your database
+        'PORT': config('PORT'),        # Leave as 3306, the default MySQL port
     }
 }
 
