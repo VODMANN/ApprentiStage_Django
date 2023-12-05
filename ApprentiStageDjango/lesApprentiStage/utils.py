ed93f53007b3cb2a0746113e651fc441f54d36b0
sami53tk <rochdi53sami@gmail.com>
2023-12-05 17:41:44 +0100

    generation de convention adapté avec info manquantes

--- ApprentiStageDjango/lesApprentiStage/utils.py
+++ ApprentiStageDjango/lesApprentiStage/utils.py
@@ -1,12 +1,25 @@
 from lesApprentiStage.models import Responsable
-from docx import Document
+from django.core.files import File
+from docx import Document as Documents
+from .models import *
+from django.core.files.base import ContentFile
+
 import os
 from django.conf import settings
 
+
+def remplacer_texte(doc, recherche, remplacement):
+    for p in doc.paragraphs:
+        if recherche in p.text:
+            for run in p.runs:
+                run.text = run.text.replace(recherche, remplacement)
+
 def generer_convention(contrat):
-    doc = Document(os.path.join(settings.BASE_DIR, '/home/rochdi/iut/v2/ApprentiStage_Django/ApprentiStageDjango/lesApprentiStage/static/MODELE_CONVENTION_STAGE.docx'))
+    doc = Documents(os.path.join(settings.BASE_DIR, '/home/rochdi/iut/v2/ApprentiStage_Django/ApprentiStageDjango/lesApprentiStage/static/MODELE_CONVENTION_STAGE.docx'))
     responsable = Responsable.objects.filter(entreprise=contrat.entreprise).first()
-    context = {
+
+    if contrat.etudiant.civiliteEtu == 'M.':
+        context = {
         '{{ nom_etudiant }}': contrat.etudiant.nomEtu,
         '{{ prenom_etudiant }}': contrat.etudiant.prenomEtu,
         '{{ nom_entreprise }}': contrat.entreprise.nomEnt,
@@ -18,6 +31,26 @@ def generer_convention(contrat):
         '{{ prenom_tuteur }}': contrat.tuteur.prenomTuteur,
         '{{ metier_tuteur }}': contrat.tuteur.metierTuteur,
         '{{ mail_tuteur }}': contrat.tuteur.emailTuteur,
+        '{{ sexeM_etu }}': 'M ☑ ',
+        '{{ sexeF_etu }}': 'F ◻ ',
+
+    }
+
+    else:
+        context = {
+        '{{ nom_etudiant }}': contrat.etudiant.nomEtu,
+        '{{ prenom_etudiant }}': contrat.etudiant.prenomEtu,
+        '{{ nom_entreprise }}': contrat.entreprise.nomEnt,
+        '{{ adresse_entreprise }}': contrat.entreprise.adresseEnt,
+        '{{ nom_responsable }}': responsable.nomResp if responsable else '',
+        '{{ email_responsable }}': responsable.emailResp if responsable else '',
+        '{{ adresse_etudiant }}': contrat.etudiant.adresseEtu,
+        '{{ nom_tuteur }}': contrat.tuteur.nomTuteur,
+        '{{ prenom_tuteur }}': contrat.tuteur.prenomTuteur,
+        '{{ metier_tuteur }}': contrat.tuteur.metierTuteur,
+        '{{ mail_tuteur }}': contrat.tuteur.emailTuteur,
+        '{{ sexeF_etu }}': '☑ F',
+        {{ 'sexeM_etu' }} : '◻ M'
     }
 
     # Remplacement dans les paragraphes
@@ -34,11 +67,22 @@ def generer_convention(contrat):
                     if key in cell.text:
                         cell.text = cell.text.replace(key, value)
 
+
+
     save_dir = os.path.join(settings.MEDIA_ROOT, 'conventions')
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    save_path = os.path.join(save_dir, f'Convention_{contrat.etudiant.nomEtu}_{contrat.etudiant.prenomEtu}.docx')
+    file_name = f'Convention_{contrat.etudiant.nomEtu}_{contrat.etudiant.prenomEtu}.docx'
+    save_path = os.path.join(settings.MEDIA_ROOT, 'documents', 'conventions', file_name)
+
     doc.save(save_path)
 
+    with open(save_path, 'rb') as file:
+        django_file = ContentFile(file.read(), name=file_name)
+        nouveau_document = Document(titre=file_name, fichier=django_file, contrat=contrat)
+        # Sauvegardez le document pour enregistrer le fichier dans MEDIA_ROOT
+        nouveau_document.save()
+
     return save_path
+
