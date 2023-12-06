a1fcabe38de4af62cfa28cc6ceab6dc6fdc7c3f9
sami53tk <90708145+sami53tk@users.noreply.github.com>
2023-12-06 14:45:45 +0100

    Update utils.py

--- ApprentiStageDjango/lesApprentiStage/utils.py
+++ ApprentiStageDjango/lesApprentiStage/utils.py
@@ -1,8 +1,11 @@
+from io import BytesIO
 from lesApprentiStage.models import Responsable
-from django.core.files import File
-from docx import Document as Documents
-from .models import *
+from docx import Document as DocxDocument
+from .models import Document
 from django.core.files.base import ContentFile
+import os
+from django.conf import settings
+
 
 import os
 from django.conf import settings
@@ -15,55 +18,44 @@ def remplacer_texte(doc, recherche, remplacement):
                 run.text = run.text.replace(recherche, remplacement)
 
 def generer_convention(contrat):
-    doc = Documents(os.path.join(settings.BASE_DIR, 'lesApprentiStage/static/MODELE_CONVENTION_STAGE.docx'))
+    # Charger le modèle de document Word
+    doc = DocxDocument(os.path.join(settings.BASE_DIR, 'lesApprentiStage/static/MODELE_CONVENTION_STAGE.docx'))
+    
+    # Récupérer le responsable de l'entreprise liée au contrat
     responsable = Responsable.objects.filter(entreprise=contrat.entreprise).first()
 
+    # Définir le contexte de remplacement
     context = {
-    '{{ nom_etudiant }}': contrat.etudiant.nomEtu,
-    '{{ prenom_etudiant }}': contrat.etudiant.prenomEtu,
-    '{{ nom_entreprise }}': contrat.entreprise.nomEnt,
-    '{{ adresse_entreprise }}': contrat.entreprise.adresseEnt,
-    '{{ nom_responsable }}': responsable.nomResp if responsable else '',
-    '{{ email_responsable }}': responsable.emailResp if responsable else '',
-    '{{ adresse_etudiant }}': contrat.etudiant.adresseEtu,
-    '{{ nom_tuteur }}': contrat.tuteur.nomTuteur,
-    '{{ prenom_tuteur }}': contrat.tuteur.prenomTuteur,
-    '{{ metier_tuteur }}': contrat.tuteur.metierTuteur,
-    '{{ mail_tuteur }}': contrat.tuteur.emailTuteur,
-    '{{ sexeM_etu }}': 'M ☑ ' if contrat.etudiant.civiliteEtu == 'M.' else 'M ◻ ',
-    '{{ sexeF_etu }}': 'F ◻ ' if contrat.etudiant.civiliteEtu == 'M.' else 'F ☑ ',
+        '{{ nom_etudiant }}': contrat.etudiant.nomEtu,
+        '{{ prenom_etudiant }}': contrat.etudiant.prenomEtu,
+        '{{ nom_entreprise }}': contrat.entreprise.nomEnt,
+        '{{ adresse_entreprise }}': contrat.entreprise.adresseEnt,
+        '{{ nom_responsable }}': responsable.nomResp if responsable else '',
+        '{{ email_responsable }}': responsable.emailResp if responsable else '',
+        '{{ adresse_etudiant }}': contrat.etudiant.adresseEtu,
+        '{{ nom_tuteur }}': contrat.tuteur.nomTuteur,
+        '{{ prenom_tuteur }}': contrat.tuteur.prenomTuteur,
+        '{{ metier_tuteur }}': contrat.tuteur.metierTuteur,
+        '{{ mail_tuteur }}': contrat.tuteur.emailTuteur,
+        '{{ sexeM_etu }}': 'M ☑ ' if contrat.etudiant.civiliteEtu == 'M.' else 'M ◻ ',
+        '{{ sexeF_etu }}': 'F ◻ ' if contrat.etudiant.civiliteEtu == 'M.' else 'F ☑ ',
     }
-    # Remplacement dans les paragraphes
-    for p in doc.paragraphs:
-        for key, value in context.items():
-            if key in p.text:
-                p.text = p.text.replace(key, value)
 
-    # Remplacement dans les tables
-    for table in doc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                for key, value in context.items():
-                    if key in cell.text:
-                        cell.text = cell.text.replace(key, value)
+    # Remplacer le texte dans les paragraphes et les tables
+    for key, value in context.items():
+        remplacer_texte(doc, key, value)
 
+    # Sauvegarder le document dans un buffer en mémoire
+    file_stream = BytesIO()
+    doc.save(file_stream)
+    file_stream.seek(0)
 
-
-    save_dir = os.path.join(settings.BASE_DIR, 'documents', 'conventions')
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
+    # Créer un ContentFile à partir du buffer
     file_name = f'Convention_{contrat.etudiant.nomEtu}_{contrat.etudiant.prenomEtu}.docx'
-    save_path = os.path.join(settings.BASE_DIR, 'documents', 'conventions', file_name)
+    django_file = ContentFile(file_stream.read(), name=file_name)
 
-    doc.save(save_path)
+    # Créer et enregistrer le nouveau document dans le modèle Document
+    nouveau_document = Document(titre=file_name, fichier=django_file, contrat=contrat)
+    nouveau_document.save()
 
-    with open(save_path, 'rb') as file:
-        # Exemple de sauvegarde de fichier dans votre fonction generer_convention
-        django_file = ContentFile(file.read(), name=file_name)
-        nouveau_document = Document(titre=file_name, fichier=django_file, contrat=contrat)
-        nouveau_document.save()
-
-
-    return save_path
-
+    return nouveau_document.fichier.path
