7232fac8440b45ebb50d236c386a8def284bfdea
Enzo <enzo.erdmann@gmail.com>
2023-12-12 17:00:43 +0100

    filtres corrigées

--- ApprentiStageDjango/lesApprentiStage/filters.py
+++ ApprentiStageDjango/lesApprentiStage/filters.py
@@ -41,71 +41,133 @@ class ProfilEtudiantFilter(filters.FilterSet):
         fields = ['nomEtu', 'prenomEtu', 'numEtu', 'promo__nomPromo', 'idDepartement__nomDep']
 
 class ProfilEnseignantFilter(filters.FilterSet):
+    numHarpege = django_filters.CharFilter(lookup_expr='icontains', label='Numéro Harpège')
+    nomEnseignant = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'enseignant')
+    prenomEnseignant = django_filters.CharFilter(lookup_expr='icontains', label='Prénom de l\'enseignant')
+    roleEnseignant = django_filters.CharFilter(lookup_expr='icontains', label='Rôle de l\'enseignant')
+    disciplineEnseignant = django_filters.CharFilter(lookup_expr='icontains', label='Discipline de l\'enseignant')
+
     class Meta:
         model = ProfilEnseignant
         fields = ['numHarpege', 'nomEnseignant', 'prenomEnseignant', 'roleEnseignant', 'disciplineEnseignant']
 
 class PromoFilter(filters.FilterSet):
+    nomPromo = django_filters.CharFilter(lookup_expr='icontains', label='Nom de la promotion')
+    annee = django_filters.CharFilter(lookup_expr='icontains', label='Année')
+    departement__nomDep = django_filters.CharFilter(lookup_expr='icontains', label='Nom du département')
+
     class Meta:
         model = Promo
         fields = ['nomPromo', 'annee', 'departement__nomDep']
 
+
+
 class EntrepriseFilter(filters.FilterSet):
-    nomEnt = django_filters.CharFilter(lookup_expr='icontains', label='Nom Entreprise')
-    mailEnt = django_filters.CharFilter(lookup_expr='icontains', label='Mail Entreprise')
+    nomEnt = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'entreprise')
+    mailEnt = django_filters.CharFilter(lookup_expr='icontains', label='Email de l\'entreprise')
     codeNaf = django_filters.CharFilter(lookup_expr='icontains', label='Code NAF')
-    villeEnt = django_filters.CharFilter(lookup_expr='icontains', label='Ville Entreprise')
-    
+    villeEnt = django_filters.CharFilter(lookup_expr='icontains', label='Ville de l\'entreprise')
+
     class Meta:
         model = Entreprise
         fields = ['nomEnt', 'mailEnt', 'codeNaf', 'villeEnt']
 
 class ThemeFilter(filters.FilterSet):
+    nomTheme = django_filters.CharFilter(lookup_expr='icontains', label='Nom du thème')
+
     class Meta:
         model = Theme
         fields = ['nomTheme']
-        
+
 class SalleFilter(filters.FilterSet):
+    numero = django_filters.CharFilter(lookup_expr='icontains', label='Numéro de la salle')
+
     class Meta:
         model = Salle
         fields = ['numero']
 
 class DepartementFilter(filters.FilterSet):
+    nomDep = django_filters.CharFilter(lookup_expr='icontains', label='Nom du département')
+    adresseDep = django_filters.CharFilter(lookup_expr='icontains', label='Adresse du département')
+
     class Meta:
         model = Departement
-        fields = ['nomDep']
+        fields = ['nomDep', 'adresseDep']
 
 class ResponsableFilter(filters.FilterSet):
+    nomResp = django_filters.CharFilter(lookup_expr='icontains', label='Nom du responsable')
+    prenomResp = django_filters.CharFilter(lookup_expr='icontains', label='Prénom du responsable')
+    emailResp = django_filters.CharFilter(lookup_expr='icontains', label='Email du responsable')
+    posteResp = django_filters.CharFilter(lookup_expr='icontains', label='Poste du responsable')
+
     class Meta:
         model = Responsable
         fields = ['nomResp', 'prenomResp', 'emailResp', 'posteResp']
 
+
+
 class TuteurFilter(filters.FilterSet):
+    nomTuteur = django_filters.CharFilter(lookup_expr='icontains', label='Nom du tuteur')
+    prenomTuteur = django_filters.CharFilter(lookup_expr='icontains', label='Prénom du tuteur')
+    metierTuteur = django_filters.CharFilter(lookup_expr='icontains', label='Métier du tuteur')
+    emailTuteur = django_filters.CharFilter(lookup_expr='icontains', label='Email du tuteur')
+
     class Meta:
         model = Tuteur
         fields = ['nomTuteur', 'prenomTuteur', 'metierTuteur', 'emailTuteur']
 
 class OffreFilter(filters.FilterSet):
+    titre = django_filters.CharFilter(lookup_expr='icontains', label='Titre de l\'offre')
+    competences = django_filters.CharFilter(lookup_expr='icontains', label='Compétences requises')
+    datePublication = django_filters.CharFilter(lookup_expr='icontains', label='Date de publication')
+    entreprise__nomEnt = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'entreprise')
+    theme__nomTheme = django_filters.CharFilter(lookup_expr='icontains', label='Nom du thème')
+
     class Meta:
         model = Offre
         fields = ['titre', 'competences', 'datePublication', 'entreprise__nomEnt', 'theme__nomTheme']
 
 class SoutenanceFilter(filters.FilterSet):
+    dateSoutenance = django_filters.CharFilter(lookup_expr='icontains', label='Date de soutenance')
+    heureSoutenance = django_filters.CharFilter(lookup_expr='icontains', label='Heure de soutenance')
+    salle__numero = django_filters.CharFilter(lookup_expr='icontains', label='Numéro de salle')
+    idContrat__etudiant__nomEtu = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'étudiant')
+    idContrat__etudiant__prenomEtu = django_filters.CharFilter(lookup_expr='icontains', label='Prénom de l\'étudiant')
+    estDistanciel = django_filters.CharFilter(lookup_expr='icontains', label='Est distanciel')
+
     class Meta:
         model = Soutenance
         fields = ['dateSoutenance', 'heureSoutenance', 'salle__numero', 'idContrat__etudiant__nomEtu', 'idContrat__etudiant__prenomEtu', 'estDistanciel']
 
 class DocumentFilter(filters.FilterSet):
+    titre = django_filters.CharFilter(lookup_expr='icontains', label='Titre du document')
+    contrat__etudiant__nomEtu = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'étudiant')
+    contrat__etudiant__prenomEtu = django_filters.CharFilter(lookup_expr='icontains', label='Prénom de l\'étudiant')
+
     class Meta:
         model = Document
         fields = ['titre', 'contrat__etudiant__nomEtu', 'contrat__etudiant__prenomEtu']
 
 class EvaluationFilter(filters.FilterSet):
+    contrat__etudiant__nomEtu = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'étudiant')
+    contrat__etudiant__prenomEtu = django_filters.CharFilter(lookup_expr='icontains', label='Prénom de l\'étudiant')
+    enseignant__nomEnseignant = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'enseignant')
+    enseignant__prenomEnseignant = django_filters.CharFilter(lookup_expr='icontains', label='Prénom de l\'enseignant')
+
     class Meta:
         model = Evaluation
         fields = ['contrat__etudiant__nomEtu', 'contrat__etudiant__prenomEtu', 'enseignant__nomEnseignant', 'enseignant__prenomEnseignant']
 
 class EtablissementFilter(filters.FilterSet):
+    nomUniversite = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'université')
+    adresseUniversite = django_filters.CharFilter(lookup_expr='icontains', label='Adresse de l\'université')
+    villeEtablissement = django_filters.CharFilter(lookup_expr='icontains', label='Ville de l\'établissement')
+    telEtablissement = django_filters.CharFilter(lookup_expr='icontains', label='Téléphone de l\'établissement')
+    mailEtablissement = django_filters.CharFilter(lookup_expr='icontains', label='Email de l\'établissement')
+    siretEtablissement = django_filters.CharFilter(lookup_expr='icontains', label='SIRET de l\'établissement')
+    nomEtablissement = django_filters.CharFilter(lookup_expr='icontains', label='Nom de l\'établissement')
+    directeurEtablissement = django_filters.CharFilter(lookup_expr='icontains', label='Directeur de l\'établissement')
+
     class Meta:
         model = Etablissement
         fields = ['nomUniversite', 'adresseUniversite', 'villeEtablissement', 'telEtablissement', 'mailEtablissement', 'siretEtablissement', 'nomEtablissement', 'directeurEtablissement']
