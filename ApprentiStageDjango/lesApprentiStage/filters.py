e903fb2a6a12b7814584a0f3de6929ede4e69881
sami53tk <rochdi53sami@gmail.com>
2023-12-12 15:01:35 +0100

    crud deb

--- ApprentiStageDjango/lesApprentiStage/filters.py
+++ ApprentiStageDjango/lesApprentiStage/filters.py
@@ -1,6 +1,7 @@
 from django_filters import rest_framework as filters
 import django_filters
 from .models import *
+from django import forms
 
 TYPE_CHOICES_CONTRAT = [
     ('Stage', 'Stage'),
@@ -17,7 +18,12 @@ ETAT_CHOICES_CONTRAT = [
 class ContratFilter(filters.FilterSet):
     type = django_filters.ChoiceFilter(choices=TYPE_CHOICES_CONTRAT, label='Type de Contrat')
     etat = django_filters.ChoiceFilter(choices=ETAT_CHOICES_CONTRAT)
-
+    etudiant__nomEtu=django_filters.CharFilter(lookup_expr='icontains')
+    promo__nomPromo = django_filters.ModelChoiceFilter(
+        queryset=Promo.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Promo'
+    )
     class Meta:
         model = Contrat
-        fields = ['type', 'etat', 'etudiant__nomEtu', 'etudiant__prenomEtu', 'entreprise__nomEnt', 'theme__nomTheme']
+        fields = ['type', 'etat', 'etudiant__nomEtu', 'etudiant__prenomEtu', 'entreprise__nomEnt', 'theme__nomTheme','promo__nomPromo']
